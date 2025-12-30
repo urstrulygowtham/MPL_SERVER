b9b608aae3a11f89c922c7582f5527b26ae7cfdb
urstrulygowtham <youremail@example.com>
2025-12-30 19:42:24 +0530

    first

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://mpl-bice.vercel.app',
   credentials: true
 }));
 app.use(express.json());
